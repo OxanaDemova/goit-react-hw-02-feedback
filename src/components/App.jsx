dcc43cec8944d09fe1f1c067ff67b97d6fa1f945
OxanaDemova <93287016+OxanaDemova@users.noreply.github.com>
2022-07-26 00:01:22 +0300

    addFeedbackOptions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,25 +7,57 @@ import { Statistics } from "./Statistics/Statistics";
 
 export class App extends Component {
   state = {
-  good: 0,
-  neutral: 0,
-  bad: 0
+    good: 0,
+    neutral: 0,
+    bad: 0
   }
+
+
+  handleGoodFeedback = () => {
+    this.setState(prevState => ({
+      good: prevState.good + 1,
+    }));
+  };
+
+  handleNeutralFeedback = () => {
+    this.setState(prevState => ({
+      neutral: prevState.neutral + 1,
+    }))
+  };
+
+  handleBadFeedback = () => {
+    this.setState(prevState => ({
+      bad: prevState.bad + 1,
+    }))
+  };
+
+  countTotalFeedback = ({ good, bad, neutral }) => {
+    
+    const totalFeedback = good + bad + neutral;
+  
+    return totalFeedback;
+  };
+  
 
 
   render() {
 
+    const { good, bad, neutral } = this.state;
+
     return (
+      
       <Container>
         <Section title={'Please leave feedback'}>
-          <Button text={'Good'} />
-          <Button text={'Neutral'} />
-          <Button text={'Bad'} />
+          <Button text={'Good'}  onIncrement={this.handleGoodFeedback}/>
+          <Button text={'Neutral'} onIncrement={this.handleNeutralFeedback}/>
+          <Button text={'Bad'} onIncrement={this.handleBadFeedback} />
           <Statistics
             title={'Statistics'}
-            good={this.state.good}
-            bad={this.state.bad}
-            neutral={this.state.neutral}
+            good={good}
+            bad={bad}
+            neutral={neutral}
+            total={this.countTotalFeedback}
+               
           />
         </Section>
       </Container>
